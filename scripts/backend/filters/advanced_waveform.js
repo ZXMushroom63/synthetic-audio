645446c8916ecfe342543e3c193d8e593274d2b5
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-13 15:51:06 +0800

    add unison??

--- scripts/backend/filters/advanced_waveform.js
+++ scripts/backend/filters/advanced_waveform.js
@@ -20,6 +20,11 @@ addBlockType("p_waveform_plus", {
         "HarmonicsRatio": [0.5, "number"],
         "HarmonicsUseSemitones": [false, "checkbox"],
         "HarmonicsSemitoneOffset": [7, "checkbox", 1],
+        "Unison": [false, "checkbox"],
+        "uVoices": [4, "number"],
+        "uAmplitudeRatio": [4, "number"],
+        "uDetuneHz": [0, "number", 1],
+        "uPan": [0.0, "number", 1],
         "Absolute": [false, "checkbox"],
         "Multiply": [false, "checkbox"],
         "Sidechain": [false, "checkbox"],
@@ -41,6 +46,13 @@ addBlockType("p_waveform_plus", {
             "HarmonicsRatio",
             "HarmonicsUseSemitones",
             "HarmonicsSemitoneOffset",
+        ],
+        "Unison": [
+            "Unison",
+            "uVoices",
+            "uAmplitudeRatio",
+            "uDetuneHz",
+            "uPan"
         ]
     },
     customGuiButtons: {
@@ -55,6 +67,12 @@ addBlockType("p_waveform_plus", {
             document.querySelector("#loopsample").play();
         },
     },
+    updateMiddleware: (loop) => {
+        if (loop.conf.Unison) {
+            loop.conf.Harmonics = false;
+            loop.querySelector("[data-key=Harmonics]").checked = false;
+        }
+    },
     functor: function (inPcm, channel, data) {
         var keys = ["Sine", "Square", "Sawtooth", "Triangle"];
         var underscores = {};
@@ -67,12 +85,24 @@ addBlockType("p_waveform_plus", {
         var exp = _(this.conf.Exponent);
         var amp = _(this.conf.Amplitude);
         var period = _(this.conf.Period);
+
         var semitones = _(this.conf.HarmonicsSemitoneOffset);
+
+        var uDetuneHz = _(this.conf.uDetuneHz);
+        var uPan = _(this.conf.uPan);
 
         var totalNormalisedVolume = 0;
         if (this.conf.Harmonics) {
-            for (let h = 0; h < this.conf.HarmonicsCount; h++) {
+            for (let h = 0; h < this.conf.HarmonicsCount + 1; h++) {
                 totalNormalisedVolume += Math.pow(this.conf.HarmonicsRatio, h);
+            }
+        } else if (this.conf.Unison) {
+            for (let h = 0; h < this.conf.uVoices / 2; h++) {
+                totalNormalisedVolume += Math.pow(this.conf.uAmplitudeRatio, h);
+            }
+            totalNormalisedVolume *= 2;
+            if (this.conf.uVoices % 2) {
+                totalNormalisedVolume -= 1; //If odd
             }
         } else {
             totalNormalisedVolume = 1;
@@ -94,26 +124,55 @@ addBlockType("p_waveform_plus", {
             var f = freq(i, inPcm);
             f *= Math.exp(-fdecay(i, inPcm) * t);
             var y = 0;
-            for (let h = 0; h < (this.conf.Harmonics ? this.conf.HarmonicsCount + 1 : 1); h++) {
-                if (t < (h * this.conf.HarmonicsStrum)) {
+            var waveCount = 1;
+            if (this.conf.Harmonics) {
+                waveCount = this.conf.HarmonicsCount + 1;
+            }
+            if (this.conf.Unison) {
+                waveCount = this.conf.uVoices;
+            }
+            var detuneHz = uDetuneHz(i, inPcm);
+            var panAmount = uPan(i, inPcm) / this.conf.uVoices;
+            var thePeriod = period(i, inPcm);
+            var semiOffset = semitones(i, inPcm);
+            for (let h = 0; h < waveCount; h++) {
+                if (t < (h * this.conf.HarmonicsStrum * this.conf.Harmonics)) {
                     continue;
                 }
-                var waveformTime = (f * t) % period(i, inPcm);
-
-                var harmonicVolumeRatio = Math.pow(this.conf.HarmonicsRatio, h);
+                var freq = f;
+                var volumeRatio = 1;
                 var coefficient = 1;
+                if (this.conf.Unison) {
+                    var detunePosition = (h + 0.5) - (waveCount / 2);
+                    freq += detuneHz * Math.trunc(detunePosition);
+                    volumeRatio = Math.abs(detunePosition);
+                    if ((waveCount % 2) === 0) {
+                        volumeRatio -= 0.5;
+                    }
+                    var panValue = panAmount * Math.trunc(detunePosition);
+                    var left = Math.min(1 - panValue, 1);
+                    var right = Math.min(1 + panValue, 1);
+                    if (channel === 0) {
+                        volumeRatio *= left;
+                    } else {
+                        volumeRatio *= right;
+                    }
+                }
+                var waveformTime = (freq * t) % thePeriod;
+
                 if (this.conf.Harmonics) {
+                    volumeRatio = Math.pow(this.conf.HarmonicsRatio, h);
                     if (this.conf.HarmonicsUseSemitones) {
-                        coefficient = getSemitoneCoefficient(Math.round(h * semitones(i, inPcm)));
+                        coefficient = getSemitoneCoefficient(Math.round(h * semiOffset));
                     } else {
                         coefficient = h + 1;
                     }
                 }
 
-                y += waveforms.sin(waveformTime * coefficient) * values.Sine * harmonicVolumeRatio;
-                y += waveforms.square(waveformTime * coefficient) * values.Square * harmonicVolumeRatio;
-                y += waveforms.sawtooth(waveformTime * coefficient) * values.Sawtooth * harmonicVolumeRatio;
-                y += waveforms.triangle(waveformTime * coefficient) * values.Triangle * harmonicVolumeRatio;
+                y += waveforms.sin(waveformTime * coefficient) * values.Sine * volumeRatio;
+                y += waveforms.square(waveformTime * coefficient) * values.Square * volumeRatio;
+                y += waveforms.sawtooth(waveformTime * coefficient) * values.Sawtooth * volumeRatio;
+                y += waveforms.triangle(waveformTime * coefficient) * values.Triangle * volumeRatio;
                 y /= total;
             }
 
