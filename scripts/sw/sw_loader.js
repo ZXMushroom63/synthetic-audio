9080e9f2bce67f35d23a7101593782e1b9b23623
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-13 09:13:16 +0800

    refactor project structure

--- scripts/sw/sw_loader.js
+++ scripts/sw/sw_loader.js
@@ -1,7 +1,7 @@
 globalThis.sw = null;
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/synthetic-audio/sw.js').then(registration => {
+        navigator.serviceWorker.register('/scripts/sw/sw_loader.js').then(registration => {
             globalThis.sw = registration;
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, err => {
