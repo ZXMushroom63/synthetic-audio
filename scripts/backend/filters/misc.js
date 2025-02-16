18572181a7392d2e2749ce28d738aca2488903b9
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-16 18:26:13 +0800

    bitcrunch fix

--- scripts/backend/filters/misc.js
+++ scripts/backend/filters/misc.js
@@ -51,11 +51,11 @@ addBlockType("bitcrunch", {
         var level = _(this.conf.Level);
         var x = Math.max(0, Math.roundsampleRateAnchor * (level(0, 0, 0)));
         for (let i = 0; i < inPcm.length; i += x + 1) {
+            x = Math.max(0, Math.round(sampleRateAnchor * level(i, inPcm)))
             var original = inPcm[i];
             for (let j = 0; j < x; j++) {
                 inPcm[i + j + 1] = original;
             }
-            x = Math.max(0, Math.round(sampleRateAnchor * level(i, inPcm)))
         }
         return inPcm;
     }
