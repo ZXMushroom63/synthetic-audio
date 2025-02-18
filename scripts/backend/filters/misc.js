f213a1047fb81ca32da46c518396108fd1a6ed3b
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-18 13:51:25 +0800

    fix stereo noise

--- scripts/backend/filters/misc.js
+++ scripts/backend/filters/misc.js
@@ -145,10 +145,15 @@ addBlockType("noise", {
     title: "Noise",
     configs: {
         "Volume": [0.5, "number", 1],
-        "Seed": [1, "number", 1]
+        "SeedLeft": [1, "number", 1],
+        "SeedRight": [1, "number", 1]
     },
     functor: function (inPcm, channel, data) {
-        Math.newRandom(this.conf.Seed);
+        if (channel === 0) {
+            Math.newRandom(this.conf.SeedLeft);
+        } else {
+            Math.newRandom(this.conf.SeedRight);
+        }
         var n = _(this.conf.Volume);
         inPcm.forEach((x, i) => {
             inPcm[i] += (Math.random() - 0.5) * n(i, inPcm) * 2;
@@ -259,7 +264,8 @@ addBlockType("smooth", {
     color: "rgba(0,255,0,0.3)",
     title: "Smooth",
     configs: {
-        "Iterations": [1, "number"]
+        "Iterations": [1, "number"],
+        "Circular": [false, "checkbox"]
     },
     functor: function (inPcm, channel, data) {
         var sampleRateAnchor = audio.samplerate / 24000;
@@ -270,8 +276,16 @@ addBlockType("smooth", {
         for (let iter = 0; iter < x; iter++) {
             let tempPcm = new Float32Array(inPcm.length);
             for (let i = 0; i < inPcm.length; i++) {
-                let prevSample = inPcm[i - 1] || inPcm[i];
-                let nextSample = inPcm[i + 1] || inPcm[i];
+                var prevSampleIdx = i - 1;
+                var nextSampleIdx = i + 1;
+                if (prevSampleIdx === -1 && this.conf.Circular) {
+                    prevSampleIdx = inPcm.length - 1;
+                }
+                if (nextSampleIdx === inPcm.length && this.conf.Circular) {
+                    nextSampleIdx = 0;
+                }
+                var prevSample = inPcm[prevSampleIdx] || inPcm[i];
+                var nextSample = inPcm[nextSampleIdx] || inPcm[i];
                 tempPcm[i] = (prevSample + inPcm[i] + nextSample) / 3;
             }
             inPcm.set(tempPcm);
