cb85aecbbda93ea922ad6da8dbf4434fa414993f
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-21 14:44:40 +0800

    add volume ratio to comb filter

--- scripts/backend/filters/misc.js
+++ scripts/backend/filters/misc.js
@@ -65,15 +65,17 @@ addBlockType("comb", {
     title: "Comb Filter",
     configs: {
         "Iterations": [1, "number"],
-        "Delay": [0.01, "number"],
+        "Delay": [0.01, "number", 1],
+        "VolumeRatio": [1, "number", "1"]
     },
     functor: function (inPcm, channel, data) {
+        var volRatio = _(this.conf.VolumeRatio);
         var delay = _(this.conf.Delay);
         var out = (new Float32Array(inPcm.length)).fill(0);
         out.forEach((x, i) => {
             var delayImpl = delay(i, out) * audio.samplerate;
             for (let j = 0; j < (this.conf.Iterations + 1); j++) {
-                out[i] += inPcm[Math.floor(delayImpl * j) + i] || 0;
+                out[i] += inPcm[Math.floor(delayImpl * j) + i] * Math.pow(volRatio(i, out), j) || 0;
             }
         });
         return out;
