580f7af59052827a5a2c1646a96f200c378f5c43
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-24 09:26:29 +0800

    add multiply and vakue filters

--- scripts/backend/filters/misc.js
+++ scripts/backend/filters/misc.js
@@ -97,6 +97,34 @@ addBlockType("quantise", {
         return inPcm;
     }
 });
+addBlockType("p_value", {
+    color: "rgba(0, 255, 225, 0.3)",
+    title: "Value",
+    configs: {
+        "Value": [0, "number", 1],
+    },
+    functor: function (inPcm, channel, data) {
+        var val = _(this.conf.Value);
+        inPcm.forEach((x, i) => {
+            inPcm[i] = val(i, inPcm);
+        });
+        return inPcm;
+    }
+});
+addBlockType("multiply", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Multiply (ADSR)",
+    configs: {
+        "Value": [0, "number", 1],
+    },
+    functor: function (inPcm, channel, data) {
+        var val = _(this.conf.Value);
+        inPcm.forEach((x, i) => {
+            inPcm[i] *= val(i, inPcm);
+        });
+        return inPcm;
+    }
+});
 addBlockType("repeat", {
     color: "rgba(0,255,0,0.3)",
     title: "Repeat",
