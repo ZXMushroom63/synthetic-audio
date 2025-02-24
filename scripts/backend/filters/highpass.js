7eefc3c773e8dedcbbb18a27d6f249bd94c6f88c
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-02-24 12:11:19 +0800

    add lowpass and highpass filter

---
+++ scripts/backend/filters/highpass.js
@@ -0,0 +1,49 @@
+async function applyHighpassFilter(pcmData, sampleRate, threshold) {
+    const offlineContext = new OfflineAudioContext(1, pcmData.length, sampleRate);
+
+    const audioBuffer = offlineContext.createBuffer(1, pcmData.length, sampleRate);
+    audioBuffer.getChannelData(0).set(pcmData);
+
+    const source = offlineContext.createBufferSource();
+    source.buffer = audioBuffer;
+
+    var thresholdValue = threshold(0, pcmData);
+
+    const bandpassFilter = offlineContext.createBiquadFilter();
+    bandpassFilter.type = 'highpass';
+    bandpassFilter.frequency.value = thresholdValue;
+
+
+    const scriptProcessor = offlineContext.createScriptProcessor(256, 1, 1);
+
+    scriptProcessor.onaudioprocess = function (audioProcessingEvent) {
+        const currentIndex = Math.floor(offlineContext.currentTime * sampleRate)
+        thresholdValue = threshold(currentIndex, pcmData);
+        bandpassFilter.frequency.value = thresholdValue;
+
+        const inputBuffer = audioProcessingEvent.inputBuffer;
+        const outputBuffer = audioProcessingEvent.outputBuffer;
+        outputBuffer.getChannelData(0).set(inputBuffer.getChannelData(0));
+    }
+
+    source.connect(bandpassFilter);
+    bandpassFilter.connect(scriptProcessor);
+    scriptProcessor.connect(offlineContext.destination);
+
+    source.start(0);
+
+    return offlineContext.startRendering().then(renderedBuffer => {
+        return renderedBuffer.getChannelData(0);
+    });
+}
+
+addBlockType("highpass", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Highpass Filter",
+    configs: {
+        "Threshold": [300, "number", 1],
+    },
+    functor: async function (inPcm, channel, data) {
+        return await applyHighpassFilter(inPcm, audio.samplerate, _(this.conf.Threshold));
+    }
+});
---
+++ scripts/backend/filters/lowpass.js
@@ -0,0 +1,49 @@
+async function applyLowpassFilter(pcmData, sampleRate, threshold) {
+    const offlineContext = new OfflineAudioContext(1, pcmData.length, sampleRate);
+
+    const audioBuffer = offlineContext.createBuffer(1, pcmData.length, sampleRate);
+    audioBuffer.getChannelData(0).set(pcmData);
+
+    const source = offlineContext.createBufferSource();
+    source.buffer = audioBuffer;
+
+    var thresholdValue = threshold(0, pcmData);
+
+    const bandpassFilter = offlineContext.createBiquadFilter();
+    bandpassFilter.type = 'lowpass';
+    bandpassFilter.frequency.value = thresholdValue;
+
+
+    const scriptProcessor = offlineContext.createScriptProcessor(256, 1, 1);
+
+    scriptProcessor.onaudioprocess = function (audioProcessingEvent) {
+        const currentIndex = Math.floor(offlineContext.currentTime * sampleRate)
+        thresholdValue = threshold(currentIndex, pcmData);
+        bandpassFilter.frequency.value = thresholdValue;
+
+        const inputBuffer = audioProcessingEvent.inputBuffer;
+        const outputBuffer = audioProcessingEvent.outputBuffer;
+        outputBuffer.getChannelData(0).set(inputBuffer.getChannelData(0));
+    }
+
+    source.connect(bandpassFilter);
+    bandpassFilter.connect(scriptProcessor);
+    scriptProcessor.connect(offlineContext.destination);
+
+    source.start(0);
+
+    return offlineContext.startRendering().then(renderedBuffer => {
+        return renderedBuffer.getChannelData(0);
+    });
+}
+
+addBlockType("lowpass", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Lowpass Filter",
+    configs: {
+        "Threshold": [300, "number", 1],
+    },
+    functor: async function (inPcm, channel, data) {
+        return await applyLowpassFilter(inPcm, audio.samplerate, _(this.conf.Threshold));
+    }
+});
