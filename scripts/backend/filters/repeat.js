9b5b11b11909d929521e4d80de4c4f16b5d3c597
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-01 13:08:33 +0800

    upgrade repeat filter

--- scripts/backend/filters/repeat.js
+++ scripts/backend/filters/repeat.js
@@ -2,20 +2,31 @@ addBlockType("repeat", {
     color: "rgba(0,255,0,0.3)",
     title: "Repeat",
     configs: {
-        "RepeatDuration": [0.1, "number"],
+        "RepeatDuration": [0.1, "number", 1],
         "FromEnd": [false, "checkbox"],
     },
     functor: function (inPcm, channel, data) {
         if (this.conf.FromEnd) {
             inPcm.reverse();
         }
-        var repeatAmount = inPcm.subarray(0, Math.floor(this.conf.RepeatDuration * audio.samplerate));
-        inPcm.forEach((x, i) => {
-            inPcm[i] = repeatAmount[i % repeatAmount.length];
-        });
-        if (this.conf.FromEnd) {
-            inPcm.reverse();
+
+        var out = new Float32Array(inPcm.length);
+
+        var repeatDuration = _(this.conf.RepeatDuration);
+
+        var repeatAmount = inPcm.subarray(0, Math.floor(repeatDuration(0, inPcm) * audio.samplerate) || 1);
+
+        for (let i = 0; i < out.length; i += repeatAmount.length) {
+            repeatAmount = inPcm.subarray(0, Math.floor(repeatDuration(i, inPcm) * audio.samplerate) || 1);
+            for (let j = 0; j < repeatAmount.length; j++) {
+                out[i + j] = repeatAmount[j];
+            }
         }
-        return inPcm;
+
+        if (this.conf.FromEnd) {
+            out.reverse();
+        }
+
+        return out;
     }
 });
