228187745701a3f1d4cf866f5fe905e0829a6fdf
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-01 19:51:33 +0800

    add exciter filter

---
+++ scripts/backend/filters/exciter.js
@@ -0,0 +1,16 @@
+addBlockType("exciter", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Exciter",
+    configs: {
+        "Mix": [0.5, "number", 1],
+    },
+    functor: function (inPcm, channel, data) {
+        var mix = _(this.conf.Mix);
+        inPcm.forEach((x, i) => {
+            var harmonicData = Math.tanh(x);
+            var mixValue = mix(i, inPcm);
+            inPcm[i] = (1 -mixValue ) * x + mixValue * harmonicData;
+        });
+        return inPcm;
+    }
+});
