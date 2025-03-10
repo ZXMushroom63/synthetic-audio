f5a1feb2fcb21878fcf30f0e95c12c875abad300
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-10 08:52:50 +0800

    add gate filter

---
+++ scripts/backend/filters/gate.js
@@ -0,0 +1,18 @@
+addBlockType("value_gate", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Gate",
+    configs: {
+        "Threshold": [0.1, "number", 1],
+        "Volume": [0, "number", 1]
+    },
+    functor: function (inPcm, channel, data) {
+        var threshold = _(this.conf.Threshold);
+        var volume = _(this.conf.Volume);
+        inPcm.forEach((x, i) => {
+            if (Math.abs(x) > threshold(i, inPcm)) {
+                inPcm[i] = x * volume(i, inPcm);
+            }
+        });
+        return inPcm;
+    }
+});
