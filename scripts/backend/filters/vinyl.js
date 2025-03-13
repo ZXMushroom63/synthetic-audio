4bf4b85a3b6578f9008e4482e407be1bd9512060
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-13 19:34:58 +0800

    Add ForceUniformSpacing to vinyl distortion

--- scripts/backend/filters/vinyl.js
+++ scripts/backend/filters/vinyl.js
@@ -3,12 +3,13 @@ addBlockType("vinyl", {
     title: "Vinyl Distortion",
     configs: {
         "Quantity": [500, "number"],
-        "SeedLeft": [1, "number"],
-        "SeedRight": [2, "number"],
+        "SeedLeft": [0, "number"],
+        "SeedRight": [0, "number"],
         "Strength": [0.5, "number", 1],
         "Down": [1, "number"],
         "Up": [1, "number"],
-        "Pitch": [1, "number"]
+        "Pitch": [1, "number"],
+        "ForceUniformSpacing": [false, "checkbox"]
     },
     functor: function (inPcm, channel, data) {
         const distribSampleRate = Math.floor(24000 * this.conf.Pitch);
@@ -16,7 +17,7 @@ addBlockType("vinyl", {
         Math.newRandom(channel === 0 ? this.conf.SeedLeft : this.conf.SeedRight);
         var strength = _(this.conf.Strength);
         for (let i = 0; i < this.conf.Quantity; i++) {
-            var position = Math.floor(Math.random() * (distribution.length - 1));
+            var position = Math.floor((this.conf.ForceUniformSpacing ? (i / this.conf.Quantity) : Math.random()) * (distribution.length - 1));
             distribution[position] = (-this.conf.Down) * strength(position, inPcm);
             distribution[position + 1] = this.conf.Up * strength(position, inPcm);
         }
