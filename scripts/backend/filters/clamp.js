7552f26870ff68203c08ffd611f1362933e69f8d
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-14 10:30:16 +0800

    add clamp filter

---
+++ scripts/backend/filters/clamp.js
@@ -0,0 +1,16 @@
+addBlockType("clamp", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Clamp",
+    configs: {
+        "Min": [-1, "number", 1],
+        "Max": [1, "number", 1],
+    },
+    functor: function (inPcm, channel, data) {
+        var min = _(this.conf.Min);
+        var max = _(this.conf.Max);
+        inPcm.forEach((x, i) => {
+            inPcm[i] = Math.min(Math.max(x, min(i, inPcm)), max(i, inPcm));
+        });
+        return inPcm;
+    }
+});
