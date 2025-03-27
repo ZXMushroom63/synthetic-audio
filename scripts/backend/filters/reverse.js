7a3eb9adcd2b55cb2bec2acc40cbcfa9708a293c
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-27 13:15:50 +0800

    add wet and dry knobs to reverse filter

--- scripts/backend/filters/reverse.js
+++ scripts/backend/filters/reverse.js
@@ -1,6 +1,7 @@
 addBlockType("reverse", {
     color: "rgba(0,255,0,0.3)",
     title: "Reverse",
+    wet_and_dry_knobs: true,
     configs: {
     },
     functor: async function (inPcm, channel, data) {
