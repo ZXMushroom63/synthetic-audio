b6fd572d6fddc99ae516f08197a90d091e6f7200
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-28 09:25:54 +0800

    fix multiline notes

--- scripts/backend/filters/note.js
+++ scripts/backend/filters/note.js
@@ -1,3 +1,6 @@
+function processNoteText(txt) {
+    return txt.split("\n").map((x, i) => (i === 0 || x === "") ? x : "---" + x).join("\n");
+}
 addBlockType("note", {
     color: "rgba(255,255,0,0.3)",
     title: "Note",
@@ -12,12 +15,12 @@ addBlockType("note", {
         var internal = loop.querySelector(".loopInternal");
         var txt = document.createElement("span");
         txt.classList.add("noteTextContent");
-        txt.innerText = loop.conf.Text;
+        txt.innerText = processNoteText(loop.conf.Text);
         internal.appendChild(txt);
         loop.setAttribute("data-nodirty", "");
         loop.querySelector(".backgroundSvg").remove();
     },
     updateMiddleware: (loop) => {
-        loop.querySelector(".noteTextContent").innerText = loop.conf.Text;
+        loop.querySelector(".noteTextContent").innerText = processNoteText(loop.conf.Text);
     }
 });
