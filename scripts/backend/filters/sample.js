69bfeadca5a8a1c5153178392f4d7baca2304630
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-03-31 17:04:15 +0800

    add sample filter

---
+++ scripts/backend/filters/sample.js
@@ -0,0 +1,17 @@
+addBlockType("sample", {
+    color: "rgba(0,255,0,0.3)",
+    title: "Sample",
+    wet_and_dry_knobs: true,
+    configs: {
+        "Position": ["#0~1", "number", 1],
+    },
+    functor: function (inPcm, channel, data) {
+        var pos = _(this.conf.Position, inPcm.length);
+        var len = inPcm.length;
+        var out = new Float32Array(len);
+        out.forEach((x, i)=>{
+            out[i] = inPcm[Math.floor(len*pos(i, out))] || 0;
+        });
+        return out;
+    }
+});
