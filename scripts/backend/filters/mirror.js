b3459e8ea14f7f27d4d1748f2ffe34f0473ffe5f
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-04-01 13:24:48 +0800

    fix mirror filter

--- scripts/backend/filters/mirror.js
+++ scripts/backend/filters/mirror.js
@@ -9,13 +9,14 @@ addBlockType("mirror", {
     functor: function (inPcm, channel, data) {
         var mid = Math.floor(WAVEFORM_RES / 2);
         var isLeft = (this.conf.Side === "LEFT");
-        var sub = isLeft ? inPcm.slice(mid, WAVEFORM_RES) : inPcm.slice(0, mid);
+        var sub = !isLeft ? inPcm.slice(mid, WAVEFORM_RES) : inPcm.slice(0, mid);
         if (this.conf.Invert) {
             sub.forEach((x, i)=>{
                 sub[i] *= -1;
             });
         }
-        inPcm.set(isLeft ? mid : 0, sub);
+        sub.reverse();
+        inPcm.set(sub, isLeft ? mid : 0);
         return inPcm;
     }
 });
