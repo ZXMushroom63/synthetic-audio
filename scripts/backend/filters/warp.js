48f818bfbc49f6a1803f3dc6ccf4eea4104c0d5f
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-04-03 09:11:03 +0800

    add chunk size to warp filter

--- scripts/backend/filters/warp.js
+++ scripts/backend/filters/warp.js
@@ -3,14 +3,23 @@ addBlockType("warp", {
     title: "Warp",
     wet_and_dry_knobs: true,
     configs: {
+        "ChunkSize": [0, "number"],
         "Position": ["#0~1", "number", 1],
     },
     functor: function (inPcm, channel, data) {
         var pos = _(this.conf.Position, inPcm.length);
         var len = inPcm.length;
+        var chunk = (this.conf.ChunkSize * audio.samplerate) || len;
         var out = new Float32Array(len);
+        var chunkPcm;
+        if (len === chunk) {
+            chunkPcm = out;
+        } else {
+            chunkPcm = new Float32Array(chunk);
+        }
         out.forEach((x, i)=>{
-            out[i] = inPcm[Math.floor(len*pos(i, out))] || 0;
+            var chunkBase = Math.floor(i / chunk) * chunk;
+            out[i] = inPcm[chunkBase + Math.floor(chunk*pos(i%chunk, chunkPcm))] || 0;
         });
         return out;
     }
