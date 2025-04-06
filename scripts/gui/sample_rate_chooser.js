2cddab6b4ad2cdfb45b34663200730932a1a9d65
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-04-06 11:32:23 +0800

    fix issue with sample rate changes

--- scripts/gui/sample_rate_chooser.js
+++ scripts/gui/sample_rate_chooser.js
@@ -2,6 +2,7 @@ addEventListener("init", ()=>{
     document.querySelector("#samplerate").addEventListener("input", (e) => {
         if (audio.samplerate !== parseInt(e.target.value)) {
             audio.samplerate = parseInt(e.target.value);
+            audio.length = audio.duration * audio.samplerate;
             decodedPcmCache = {};
             proceduralAssets.clear();
             findLoops(".loop").forEach(x => x.setAttribute("data-dirty", "yes"));
