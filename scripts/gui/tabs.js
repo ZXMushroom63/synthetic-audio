8163eaded62ea196d215c4e006b68321f395e9c3
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-05-02 10:11:35 +0800

    add tab overflow + unused metro sfx

--- scripts/gui/tabs.js
+++ scripts/gui/tabs.js
@@ -1,4 +1,6 @@
 var CURRENT_TAB = "";
+var registeredTabCounter = 0;
+const maxVisibleTabs = 2;
 function registerTab(tabName, element, selected, listener = ()=>{}) {
     var tab_code = tabName.toUpperCase().trim().replaceAll(" ", "_");
     var tab = document.createElement("span");
@@ -25,7 +27,19 @@ function registerTab(tabName, element, selected, listener = ()=>{}) {
         tab.classList.add("selected");
         CURRENT_TAB = tab_code;
     });
-    document.querySelector("#controlsAndStuff").appendChild(tab);
+    if (registeredTabCounter === maxVisibleTabs) {
+        const overflowTab = document.createElement("span");
+        overflowTab.innerText = "...";
+        overflowTab.classList.add("tab");
+        overflowTab.id = "overflowTab";
+        document.querySelector("#controlsAndStuff").appendChild(overflowTab);
+    }
+    if (registeredTabCounter < maxVisibleTabs) {
+        document.querySelector("#controlsAndStuff").appendChild(tab);
+    } else {
+        document.querySelector("#overflowTab").appendChild(tab);
+    }
+    registeredTabCounter++;
 }
 addEventListener("init", ()=>{
     registerTab("Timeline", document.querySelector("#track"), true);
