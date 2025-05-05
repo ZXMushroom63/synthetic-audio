0e634be3d35c3f2d185462590cdb006b6b185535
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-05-05 11:41:25 +0800

    add offset negative option to powers

--- scripts/backend/filters/power.js
+++ scripts/backend/filters/power.js
@@ -3,12 +3,17 @@ addBlockType("power", {
     title: "Power",
     wet_and_dry_knobs: true,
     configs: {
-        "Exponent": [1.5, "number", 1]
+        "Exponent": [1.5, "number", 1],
+        "OffsetNegatives": [false, "checkbox"]
     },
     functor: function (inPcm, channel, data) {
         var exp = _(this.conf.Exponent);
         inPcm.forEach((x, i) => {
-            inPcm[i] = Math.pow(Math.abs(x), exp(i, inPcm)) * Math.sign(x);
+            if (this.conf.OffsetNegatives) {
+                inPcm[i] = Math.pow((x + 1) / 2, exp(i, inPcm)) * 2 - 1;
+            } else {
+                inPcm[i] = Math.pow(Math.abs(x), exp(i, inPcm)) * Math.sign(x);
+            }
         });
         return inPcm;
     }
