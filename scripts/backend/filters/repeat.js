d5aabb69d4a08a626ca815aa501617f0fa9a6043
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-02 13:32:23 +0800

    add amplitude smoothing to repeat node

--- scripts/backend/filters/repeat.js
+++ scripts/backend/filters/repeat.js
@@ -5,9 +5,12 @@ addBlockType("repeat", {
     configs: {
         "RepeatDuration": [0.1, "number", 1],
         "FromEnd": [false, "checkbox"],
-        "WrapExcessDataSize": [0, "number"]
+        "WrapExcessDataSize": [0, "number"],
+        "AmpSmoothingStart": [0, "number"],
+        "AmpSmoothingEnd": [0, "number"]
     },
     functor: function (inPcm, channel, data) {
+        inPcm = inPcm.slice();
         if (this.conf.FromEnd) {
             inPcm.reverse();
         }
@@ -16,13 +19,27 @@ addBlockType("repeat", {
 
         var repeatDuration = _(this.conf.RepeatDuration);
         var startSample = Math.floor(repeatDuration(0, inPcm) * audio.samplerate) || 1;
-        var repeatAmount = inPcm.subarray(0, startSample);
+        var repeatAmount = inPcm.slice(0, startSample);
+        repeatAmount.forEach((x, i) => {
+            const AmpSmoothingStart = Math.floor(audio.samplerate * this.conf.AmpSmoothingStart);
+            const AmpSmoothingEnd = inPcm.length - Math.floor(audio.samplerate * this.conf.AmpSmoothingEnd);
+
+            var ampSmoothingFactor = 1;
+            if (i < AmpSmoothingStart) {
+                ampSmoothingFactor *= i / AmpSmoothingStart;
+            }
+
+            if (i > AmpSmoothingEnd) {
+                ampSmoothingFactor *= 1 - ((i - AmpSmoothingEnd) / AmpSmoothingStart);
+            }
+            data[i] *= ampSmoothingFactor;
+        })
         const excess = inPcm.subarray(startSample, startSample + Math.floor(this.conf.WrapExcessDataSize * audio.samplerate));
         excess.forEach((x, i) => {
             repeatAmount[i % startSample] += x;
         });
         for (let i = 0; i < out.length; i += repeatAmount.length) {
-            repeatAmount = inPcm.subarray(0, Math.floor(repeatDuration(i, inPcm) * audio.samplerate) || 1);
+            repeatAmount = inPcm.slice(0, Math.floor(repeatDuration(i, inPcm) * audio.samplerate) || 1);
             for (let j = 0; j < repeatAmount.length; j++) {
                 out[i + j] = repeatAmount[j];
             }
