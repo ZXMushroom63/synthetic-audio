97e7849c10d1aee2a80f376909b07775e3a56b6d
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-03 15:55:00 +0800

    fix sample packs bind

--- scripts/tools/samplepacks.js
+++ scripts/tools/samplepacks.js
@@ -125,7 +125,7 @@ addEventListener("init", () => {
         resetDrophandlers(true);
         activateTool("MOVE");
         samplepackMenu.init();
-    }, false, (e) => e.shiftKey && e.key === "s");
+    }, false, (e) => e.shiftKey && e.key === "S");
 
     document.querySelector("#samplepacksBtn").addEventListener("click", () => { TOOL_DATABASE["SAMPLES"](null); });
 });
