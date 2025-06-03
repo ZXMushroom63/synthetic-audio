cace153316aef7827934bce37f37e65bf7bbfcb4
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-03 10:00:10 +0800

    fix samplepacks

--- scripts/tools/samplepacks.js
+++ scripts/tools/samplepacks.js
@@ -74,7 +74,7 @@ addEventListener("init", () => {
         });
 
         Object.keys(loopMap).filter(x => x.startsWith(path)).forEach(x => {
-            const display = `${x.replace(path, "")} [${loopDurationMap[path].toFixed(2)}s]`;
+            const display = `${x.replace(path, "")} [${loopDurationMap[x].toFixed(2)}s]`;
             if (display.includes("/")) {
                 return;
             }
