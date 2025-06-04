bee418076ba78c127c7458db9b0953d52888098a
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-04 11:59:00 +0800

    add two new bonus arp patterns

---
+++ extras/arp/bonus-lavender-town.arp.js
@@ -0,0 +1,3 @@
+
+    ARPEGGIATOR_SCORES["bonus/lavender-town"] = {"notes":[{"semis":0,"beatsStart":0,"beatsDuration":2},{"semis":7,"beatsStart":2,"beatsDuration":2},{"semis":11,"beatsStart":4,"beatsDuration":2},{"semis":6,"beatsStart":6,"beatsDuration":2}]};
+    
