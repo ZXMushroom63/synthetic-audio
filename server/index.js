95583a696c3052ff2a5eedc35a8e5a615a68fec7
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-15 17:44:45 +0800

    upgrade multiplayer setup

--- server/index.js
+++ server/index.js
@@ -2,13 +2,32 @@ const express = require('express');
 const { createServer } = require("node:http");
 const { multiplayer_support } = require("./multiplayer.js");
 const cors = require('cors');
+const { readFile } = require("fs/promises");
 const app = express();
 const server = createServer(app);
-app.use(express.static('.'));
-app.use(cors({origin: "*"}))
-const multiplayerEnabled = true;
+
+
+
+// CONFIGS
+const multiplayerEnabled = 2;
+// 0: multiplayer disabled
+// 1: multiplayer enabled, client frontent that autoconnects to server. not recommended is the server isn't static, as service workers will cache new clients repeatedly.
+// 2: multiplayer enabled, and client frontent is disabled.
+
 const port = 80;
 const logging = false;
+
+
+if (multiplayerEnabled !== 2) {
+    app.use(express.static('.'));
+} else {
+    app.get('/', async (req, res) => {
+        res.status(200).send(await readFile("./server/multiplayer_notice.html", {encoding: "utf8"}));
+    });
+}
+app.use(cors({ origin: "*" }))
+
+
 const blacklistMiddleware = (req, res, next) => {
     const requestedPath = req.path;
     if (requestedPath.startsWith(`/server/`)) {
@@ -19,14 +38,14 @@ const blacklistMiddleware = (req, res, next) => {
 };
 
 app.get('/multiplayer_check', (req, res) => {
-    if (multiplayerEnabled) {
+    if (multiplayerEnabled !== 0) {
         res.status(200).send('multiplayer is supported.');
     } else {
         res.status(404).send('multiplayer not supported.');
     }
 });
 
-if (multiplayerEnabled) {
+if (multiplayerEnabled !== 0) {
     multiplayer_support(server, logging);
 }
 
