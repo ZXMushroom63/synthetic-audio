4f64309c75ad80176e253a08234a05d95df1668a
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-18 12:23:10 +0800

    add pdf printing to sheet tool

--- scripts/tools/sheet.js
+++ scripts/tools/sheet.js
@@ -8,12 +8,27 @@ addEventListener("init", () => {
         <label>Center Octave: </label><input type="number" id="sheetOctave" value="5" class="inputStyles"><br>
         <label>Key Signature: </label><input type="text" id="sheetKey" value="C major" class="inputStyles"><br>
         <label>Jazz Chords: </label><input type="checkbox" id="sheetJazzChords" class="inputStyles"><br>
-        <label>Guitar Tablature: </label><input type="checkbox" id="sheetTablature" class="inputStyles"><br>
+        <label>Measures Per Line: </label><input type="number" id="sheetTimeMPL" value="4" class="inputStyles"><br>
+        <label>Minimum Spacing: </label><input type="number" id="sheetTimeMinSpc" value="1.8" class="inputStyles"><br>
+        <label>Maximum Spacing: </label><input type="number" id="sheetTimeMaxSpc" value="2.7" class="inputStyles"><br>
+        <label>Tablature: </label><input type="checkbox" id="sheetTablature" class="inputStyles"><br>
+        <label>Tablature Mode: </label><select id="sheetTabMode">
+            <option>guitar</option>
+            <option>violin</option>
+            <option>mandolin</option>
+            <option>fiddle</option>
+            <option>fiveString</option>
+            <option value="">none</option>
+        </select><br>
+        <label>Tablature Label: </label><input type="text" id="sheetTabTitle" class="inputStyles" value="Guitar (%T)"><br>
+        <label>Tablature Tuning: </label><input type="text" id="sheetTabTuning" class="inputStyles" value="D3,A3,D4,G4,A4,D5"><br>
+        <label>Tablature Only: </label><input type="checkbox" id="sheetTabOnly" class="inputStyles"><br>
         <button id="sheetGoButton">Generate</button><br>
         <small>Note: substepping levels not equal to 1, 2, or 4 are unstable.</small>
     `);
     tabs.addTab("Sheet", `
-        <div id="sheetAbcJsTarget"></div>
+        <small>Right click -> Print... to print as a .pdf</small><br>
+        <div id="sheetAbcJsTarget" class="printable"></div>
     `);
 
     function midiToAbcNote(midiNumber) {
@@ -107,16 +122,39 @@ addEventListener("init", () => {
             staffwidth: 780, // Adjust width
             responsive: 'resize',
             selectionColor: "#000000",
-            jazzchords: document.querySelector("#sheetJazzChords").checked
+            jazzchords: document.querySelector("#sheetJazzChords").checked,
+            wrap: {
+                preferredMeasuresPerLine: document.querySelector("#sheetTimeMPL").value,
+                minSpacing: document.querySelector("#sheetTimeMinSpc").value,
+                maxSpacing: document.querySelector("#sheetTimeMaxSpc").value,
+            }
         };
         if (document.querySelector("#sheetTablature").checked) {
+            const tuning = document.querySelector("#sheetTabTuning").value.split(",").map(x => midiToAbcNote(chromaticToIndex(x.trim().toUpperCase())+12));
+            console.log(tuning);
             flags.tablature = [{
-                instrument: "guitar",
-                label: "Guitar (%T)",
-                tuning: ["D,", "A,", "D", "G", "A", "d"],
+                instrument: document.querySelector("#sheetTabMode").value,
+                label: document.querySelector("#sheetTabTitle").value,
+                tuning: tuning,
             }];
+            delete flags.wrap;
         }
         ABCJS.renderAbc('sheetAbcJsTarget', generateAbcString(parsedNotes, document.querySelector("#sheetTimeSig").value), flags);
+        const tabOnly = document.querySelector("#sheetTabOnly").checked;
+        document.querySelector("#sheetAbcJsTarget").setAttribute("data-abcjs-disable-sheet", tabOnly);
+        if (tabOnly) {
+            const svg = document.querySelector("#sheetAbcJsTarget svg");
+            const g = document.querySelector("#sheetAbcJsTarget svg>g:last-child");
+            g.children[0].remove();
+            while (g.children[0].hasAttribute("data-name")) {
+                g.children[0].remove();
+            }
+            const title = document.querySelector("#sheetAbcJsTarget svg text[data-name=title]");
+            const vb = svg.getAttribute("viewBox").split(" ");
+            vb[1] = title.getAttribute("y");
+            svg.setAttribute("viewBox", vb.join(" "));
+            title.setAttribute("y", parseFloat(title.getAttribute("y"))*2 + 20);
+        }
         MODMENU_OpenTab(null, "Sheet");
     }
 
