8a136c9a08eaea429fdb0c674615aa653e44b872
ZXMushroom63 <zxmushroom63@gmail.com>
2025-06-20 20:03:47 +0800

    add sample features to procedural assets

--- scripts/backend/filters/procedural_asset.js
+++ scripts/backend/filters/procedural_asset.js
@@ -4,9 +4,13 @@ addBlockType("p_readasset", {
     title: "Play Asset",
     directRefs: ["read"],
     configs: {
+        "Note": [":A4:", "number", 1],
         "Asset": ["(none)", ["(none)"]],
         "StartOffset": [0, "number"],
         "Volume": [1, "number"],
+        "FadeTime": [0, "number"],
+        "SamplerEnabled": [false, "checkbox"],
+        "ReferenceNote": [":A4:", "number", 1],
         "Looping": [true, "checkbox"],
         "Reverse": [false, "checkbox"],
         "Speed": [1, "number"],
@@ -14,6 +18,7 @@ addBlockType("p_readasset", {
         "SidechainPower": [2, "number"],
         "Silent": [false, "checkbox"],
     },
+    dropdowns: { "Sampler": ["SamplerEnabled", "ReferenceNote"] },
     assetUser: true,
     selectMiddleware: (key) => {
         if (key === "Asset") {
@@ -28,17 +33,65 @@ addBlockType("p_readasset", {
         var newTitle = "Asset - " + loop.conf.Asset;
         loop.setAttribute("data-file", newTitle);
         loop.querySelector(".loopInternal .name").innerText = newTitle;
+        if (loop.conf.SamplerEnabled) {
+            if (loop.querySelector(".noteDisplay")) {
+                updateNoteDisplay(loop);
+            } else {
+                initNoteDisplay(loop);
+                addChordDisplay(loop);
+            }
+        } else {
+            if (loop.querySelector(".noteDisplay")) {
+                loop.querySelector(".noteDisplay").remove();
+                loop.querySelector(".chordDisplay").remove();
+            }
+        }
+    },
+    initMiddleware: function (loop) {
+        filters["p_readasset"].updateMiddleware(loop);
+    },
+    pitchZscroller: true,
+    midiMappings: {
+        note: "Note",
+        velocity: "Volume",
+        zero: []
+    },
+    zscroll: (loop, value) => {
+        if (!loop.querySelector(".noteDisplay")) {
+            return;
+        }
+        loop.conf.Note = ":" + frequencyToNote(_(loop.conf.Note)(0, new Float32Array(1)) * Math.pow(2, value / 12)) + ":";
+        updateNoteDisplay(loop);
+
+        if (!globalThis.zscrollIsInternal && globalThis.zscrollIsFirst) {
+            filters["p_readasset"].customGuiButtons.Preview.apply(loop, []);
+        }
     },
     functor: function (inPcm, channel, data) {
-        var currentData = proceduralAssets.has(this.conf.Asset) ? proceduralAssets.get(this.conf.Asset)[channel] : [];
+        var currentData = proceduralAssets.has(this.conf.Asset) ? proceduralAssets.get(this.conf.Asset)[channel].slice() : new Float32Array(0);
+        const FADETIME = Math.min(this.conf.FadeTime * audio.samplerate, Math.min(inPcm.length, currentData.length));
+        const FADESTART = Math.min(inPcm.length, currentData.length) - FADETIME;
+        const tail = currentData.subarray(FADESTART);
+        tail.forEach((x, i) => {
+            tail[i] = x * (1 - i / FADETIME);
+        });
         var duration = Math.floor(Math.round((((currentData.length / audio.samplerate) || 0) + 0.0) / data.loopInterval) * data.loopInterval * audio.samplerate);
+        const empty = new Float32Array(2);
+        const speed = this.conf.Speed * (_(this.conf.Note)(0, empty) / _(this.conf.ReferenceNote)(0, empty));
         if (this.conf.Sidechain) {
-            applySoundbiteToPcmSidechain(this.conf.Reverse, this.conf.Looping, currentData, inPcm, duration, this.conf.Speed, this.conf.Volume, this.conf.StartOffset, this.conf.SidechainPower, this.conf.Silent);
+            applySoundbiteToPcmSidechain(this.conf.Reverse, this.conf.Looping, currentData, inPcm, duration, speed, this.conf.Volume, this.conf.StartOffset, this.conf.SidechainPower, this.conf.Silent);
         } else {
-            applySoundbiteToPcm(this.conf.Reverse, this.conf.Looping, currentData, inPcm, duration, this.conf.Speed, this.conf.Volume, this.conf.StartOffset);
+            applySoundbiteToPcm(this.conf.Reverse, this.conf.Looping, currentData, inPcm, duration, speed, this.conf.Volume, this.conf.StartOffset);
         }
 
         return inPcm;
+    },
+    customGuiButtons: {
+        "Preview": async function () {
+            var pcmData = filters["p_readasset"].functor.apply(this, [new Float32Array(audio.samplerate), 0, {}]);
+            var blob = await convertToFileBlob([sumFloat32ArraysNormalised([pcmData])], 1, audio.samplerate, audio.bitrate, true);
+            playSample(blob);
+        },
     }
 });
 addBlockType("p_writeasset", {
@@ -60,7 +113,7 @@ addBlockType("p_writeasset", {
         } else {
             proceduralAssets.get(this.conf.Asset)[channel] = inPcm;
         }
-        
+
         var out = new Float32Array(inPcm.length);
         if (this.conf.Transparent) {
             out.set(inPcm);
