cef742e7b5d41a3f5ff739407d610e4f9cfa5a19
ZXMushroom63 <zxmushroom63@gmail.com>
2025-07-01 19:54:55 +0800

    fix gzsynth previews

--- scripts/backend/filters/gzsynth.js
+++ scripts/backend/filters/gzsynth.js
@@ -115,7 +115,7 @@
         customGuiButtons: {
             "Preview": async function () {
                 var pcmData = await gzsynth.functor.apply(this, [new Float32Array(audio.samplerate), 0, {}]);
-                var blob = await convertToFileBlob([sumFloat32ArraysNormalised([pcmData])], 1, audio.samplerate, audio.bitrate, true);
+                var blob = await convertToFileBlob([sumFloat32Arrays([pcmData])], 1, audio.samplerate, audio.bitrate, true);
                 playSample(blob);
             },
         },
