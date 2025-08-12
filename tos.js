884f237b8e24cae0b2ee7405f1060036cc985ce6
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-12 16:46:22 +0800

    Add back button to ToS

--- tos.js
+++ tos.js
@@ -1,7 +1,10 @@
 addEventListener("load", async () => {
-  if (location.protocol === "file:") {
-    document.querySelector("i").innerText = (await (await fetch("https://zxmushroom63.github.io/synthetic-audio/LICENSE")).text()).trim();
-  } else {
-    document.querySelector("i").innerText = (await (await fetch("LICENSE")).text()).trim();
+  if (location.pathname.includes("terms_of_use.html")) {
+    if (location.protocol === "file:") {
+      document.querySelector("i").innerText = (await (await fetch("https://zxmushroom63.github.io/synthetic-audio/LICENSE")).text()).trim();
+    } else {
+      document.querySelector("i").innerText = (await (await fetch("LICENSE")).text()).trim();
+    }
   }
+  document.querySelector("#back").addEventListener("click", (e)=>{e.preventDefault();history.back();});
 });
