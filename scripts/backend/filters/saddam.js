f62999344399fc53abb5df7fe173f4606c2dabaa
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-18 15:46:55 +0800

    Add saddam hussein

---
+++ scripts/backend/filters/saddam.js
@@ -0,0 +1,20 @@
+addBlockType("hussein", {
+    color: "transparent",
+    title: "Saddam Hussein",
+    configs: {
+        
+    },
+    noRender: true,
+    initMiddleware: (loop)=>{
+        const internal = loop.querySelector(".loopInternal");
+        internal.style.backgroundImage = "url(public/saddam.png)";
+        internal.style.backgroundRepeat = "no-repeat";
+        internal.style.backgroundSize = "100% 100%";
+        internal.style.backgroundPosition = "centre";
+        loop.setAttribute("data-nodirty", "");
+        loop.querySelector(".backgroundSvg").remove();
+    },
+    functor: async function (inPcm, channel, data) {
+        return inPcm;
+    }
+});
