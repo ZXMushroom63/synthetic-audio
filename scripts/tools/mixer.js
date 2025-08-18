50a2c6bd3f3cacd0767ef7caf0ae2007acb61167
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-18 16:17:05 +0800

    fix panning exponentiation

--- scripts/tools/mixer.js
+++ scripts/tools/mixer.js
@@ -74,7 +74,7 @@ addEventListener("init", () => {
             panSlider.value = values.Pan;
             panSlider.setAttribute("data-panslider", "");
             panSlider.addEventListener("input", () => {
-                values.Pan = Math.pow(parseFloat(panSlider.value), 2);
+                values.Pan = Math.sign(parseFloat(panSlider.value)) * Math.pow(parseFloat(panSlider.value), 2);
                 triggerUpdate(x, values);
             });
             pan.appendChild(panSlider);
