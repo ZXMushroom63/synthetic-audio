56a4a420a1620227c172ff082c8948e7cb922397
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-18 11:33:39 +0800

    zoom centres at mouse position

--- scripts/gui/gui.js
+++ scripts/gui/gui.js
@@ -718,6 +718,7 @@ function init() {
     });
     var zoomActuatorDebouncer = null;
     var prezoomLeftVal = null;
+    var zoomCentre = innerWidth / 2;
     function actuateZoom() {
         prezoomLeftVal = null;
         zoomActuatorDebouncer = null;
@@ -731,14 +732,15 @@ function init() {
             e.stopImmediatePropagation();
             e.stopPropagation();
             if (!prezoomLeftVal) {
-                prezoomLeftVal = document.querySelector("#track").scrollLeft + innerWidth / 2;
+                zoomCentre = mouse.x;
+                prezoomLeftVal = document.querySelector("#track").scrollLeft + zoomCentre;
             }
             gui.zoom += e.deltaY * (keymap["Shift"] ? settings.ZoomScale * 0.05 : settings.ZoomScale);
             gui.zoom = Math.max(100, gui.zoom);
             document.querySelector("#track").style.willChange = "scroll-position, transform";
             const factor = gui.zoom / gui.lastHydratedZoom;
             document.querySelector("#trackInternal").style.transform = `scaleX(${factor})`;
-            document.querySelector("#track").scrollLeft = prezoomLeftVal * factor - innerWidth / 2;
+            document.querySelector("#track").scrollLeft = prezoomLeftVal * factor - zoomCentre;
             if (zoomActuatorDebouncer) {
                 clearTimeout(zoomActuatorDebouncer);
             }
