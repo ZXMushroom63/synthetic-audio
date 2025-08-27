fd56b9019d89e3c3dcd49df2b7fcb1eaebb51ef5
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-27 14:40:05 +0800

    add toasts for data transfers

--- scripts/gui/save_music_to_file.js
+++ scripts/gui/save_music_to_file.js
@@ -1,6 +1,10 @@
 addEventListener("load", () => {
-    document.querySelector("#saveAudioFile").addEventListener("click", () => {
+    document.querySelector("#saveAudioFile").addEventListener("click", async () => {
         const fname = renderFileName || "song.wav";
+        if (!renderBlob) {
+            toast("Song not rendered yet!");
+            return;
+        }
         alert("Save Prompt", `
             <span>Drag and Drop onto your desktop to save!</span><br>
             <div title="${fname}" draggable="true" class="dragAndDropTarget">${fname}</div><br>
@@ -10,9 +14,13 @@ addEventListener("load", () => {
             const dragTarget = container.querySelector(".dragAndDropTarget");
             const mime = renderCodec.mime;
             dragTarget.ondragstart = (e) => {
-                e.dataTransfer.setData(mime, renderBlob);
+                //e.dataTransfer.setData(mime, renderBlob);
                 e.dataTransfer.setData("DownloadURL", mime + ":" + fname + ":" + url);
-                e.dataTransfer.setData("text/plain", "Download Song File");
+                e.dataTransfer.setData("text/uri-list", url);
+                toast("Initiating DataTransfer");
+            };
+            dragTarget.ondragend = (e) => {
+                toast("Completed DataTransfer");
             };
         });
     });
