934cab6b2c76b454e82e1d345c965af0f06d91e7
ZXMushroom63 <zxmushroom63@gmail.com>
2025-08-29 09:14:49 +0800

    make music save menu update faster

--- scripts/gui/save_music_to_file.js
+++ scripts/gui/save_music_to_file.js
@@ -5,15 +5,17 @@ addEventListener("load", () => {
             toast("Song not rendered yet!");
             return;
         }
-        alert("Save Prompt", `
+        let handler = null;
+        await alert("Save Prompt", `
             <span>Drag and Drop onto your desktop to save!</span><br>
             <div title="${fname}" draggable="true" class="dragAndDropTarget">${fname}</div><br>
             <small>May be broken in Discord!</small>
         `, (container) => {
-            const url = document.querySelector("#renderOut").src;
             const dragTarget = container.querySelector(".dragAndDropTarget");
-            const mime = renderCodec.mime;
+            
             dragTarget.ondragstart = (e) => {
+                const url = document.querySelector("#renderOut").src;
+                const mime = renderCodec.mime;
                 //e.dataTransfer.setData(mime, renderBlob);
                 e.dataTransfer.setData("DownloadURL", mime + ":" + fname + ":" + url);
                 e.dataTransfer.setData("text/uri-list", url);
@@ -22,6 +24,13 @@ addEventListener("load", () => {
             dragTarget.ondragend = (e) => {
                 toast("Completed DataTransfer");
             };
+            handler = ()=>{
+                dragTarget.innerText = renderFileName || "song.wav";
+            };
+            console.log("registered handler");
+            addEventListener("render", handler);
         });
+        console.log("removed handler");
+        removeEventListener("render", handler)
     });
 });
