9ba6024ba2b0a2c754afa454fcdc670cbe7b5c6d
ZXMushroom63 <zxmushroom63@gmail.com>
2025-09-06 16:54:01 +0800

    add visual updates to reader mode

--- scripts/backend/filters/automation_param.js
+++ scripts/backend/filters/automation_param.js
@@ -6,7 +6,12 @@ function automationParamHandler(loop) {
     var newTitle = "@" + loop.conf.Identifier + " - Automation Parameter";
     loop.setAttribute("data-file", newTitle);
     loop.querySelector(".loopInternal .name").innerText = newTitle;
-    loop.querySelector(".genericDisplay").innerText = ("" + loop.conf.Value).startsWith("#") ? loop.conf.Value.replace("#", "") : loop.conf.Value;
+
+    if (loop.conf.ReaderMode) {
+        loop.querySelector(".genericDisplay").innerText = "Signal Reader";
+    } else {
+        loop.querySelector(".genericDisplay").innerText = ("" + loop.conf.Value).startsWith("#") ? loop.conf.Value.replace("#", "") : loop.conf.Value;
+    }
 }
 addBlockType("automation_parameter", {
     color: "rgba(255, 0, 119, 0.42)",
@@ -15,6 +20,7 @@ addBlockType("automation_parameter", {
     configs: {
         "Identifier": ["Param", "text"],
         "Value": ["#0~1", "number", 1],
+        "Transparent": [true, "checkbox"],
         "ReaderMode": [false, "checkbox"],
         "ReaderRMSFreq": [31, "number"],
         "ReaderMapping": ["#0~1", "number", 1],
@@ -74,6 +80,10 @@ addBlockType("automation_parameter", {
                 paramPcm[offset + i] = val(i, inPcm);
             }
         });
+
+        if (!this.conf.Transparent) {
+            return new Float32Array(inPcm.length);
+        }
         return inPcm;
     }
 });
