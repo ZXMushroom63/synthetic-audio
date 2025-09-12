a73a9ef8053d01df617c0be0a6f109bf7082d3fd
ZXMushroom63 <zxmushroom63@gmail.com>
2025-09-12 09:14:21 +0800

    increase number of supported keys

--- scripts/tools/cut.js
+++ scripts/tools/cut.js
@@ -58,7 +58,7 @@ addEventListener("init", () => {
 
     }, false, (e) => {
         return (e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey && e.key === "b")
-            || (e.altKey && !e.ctrlKey && !e.shiftKey && !e.metaKey && ["2", "3", "4"].includes(e.key))
+            || (e.altKey && !e.ctrlKey && !e.shiftKey && !e.metaKey && ["2", "3", "4", "5", "6", "7", "8"].includes(e.key))
     });
 });
 registerHelp(".tool[data-tool=CUT]",
