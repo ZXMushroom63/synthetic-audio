dab0f401870835c20fce3940e9d0a02e09f75afa
ZXMushroom63 <zxmushroom63@gmail.com>
2025-11-22 00:47:34 +0800

    add note display to obxd

--- obxd/obxd/midi.js
+++ obxd/obxd/midi.js
@@ -1,8 +1,9 @@
 const midiMap = {};
 var midiPort;
-
+let display = document.getElementById("midiInfo");
 function initMidi() {
   let combo = document.getElementById("midiIn");
+  display = document.getElementById("midiInfo");
   navigator.requestMIDIAccess().then((midiIF) => {
     for (let input of midiIF.inputs.values()) {
       let option = new Option(input.name);
@@ -15,10 +16,24 @@ function initMidi() {
       midiPort.onmidimessage = onMIDI;
     }
     if (combo.options.length > 0)
-      combo.onchange({ target:combo });
-  })  
+      combo.onchange({ target: combo });
+  });
 }
-
-function onMIDI (msg) {
-  obxd.port.postMessage({ type:"midi", data:msg.data });
+function isNoteOn(statusByte, vel) {
+  return (statusByte & 0xF0) === 0x90 && vel !== 0;
+}
+const pressedKeys = new Set();
+function onMIDI(msg) {
+  if (isNoteOn(msg.data[0], msg.data[2])) {
+    pressedKeys.add(msg.data[1]);
+  } else {
+    pressedKeys.delete(msg.data[1]);
+  }
+  
+  if (display) {
+    display.innerText = [...pressedKeys].sort().map((k)=>
+    window.parent.indexToChromatic(k - 12)
+  ).join(",");
+  }
+  obxd.port.postMessage({ type: "midi", data: msg.data });
 }
