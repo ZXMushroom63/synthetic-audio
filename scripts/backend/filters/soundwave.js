e0095f7490249adc945a482cc62ee2afaa63bc56
ZXMushroom63 <zxmushroom63@gmail.com>
2026-02-23 11:57:21 +0800

    Add phase setting to soundwave

--- scripts/backend/filters/soundwave.js
+++ scripts/backend/filters/soundwave.js
@@ -5,6 +5,7 @@ addBlockType("p_sinewave", {
     configs: {
         "Amplitude": [1, "number"],
         "Frequency": [100, "number"],
+        "Phase": [0, "number"],
         "Waveform": ["Sine", ["Sine", "Square", "Sawtooth", "Triangle"]],
         "Exponent": [1, "number"],
         "CustomWaveformModifier": [false, "checkbox"]
@@ -14,20 +15,21 @@ addBlockType("p_sinewave", {
         if (this.conf.CustomWaveformModifier) {
             f = audio.samplerate / WAVEFORM_RES * this.conf.Frequency;
         }
+        const p = this.conf.Phase || 0;
         inPcm.forEach((x, i) => {
             var outValue = 0;
             switch (this.conf.Waveform) {
                 case "Sine":
-                    outValue = waveforms.sin(i / audio.samplerate * f) * this.conf.Amplitude;
+                    outValue = waveforms.sin(i / audio.samplerate * f + p) * this.conf.Amplitude;
                     break;
                 case "Square":
-                    outValue = waveforms.square(i / audio.samplerate * f) * this.conf.Amplitude;
+                    outValue = waveforms.square(i / audio.samplerate * f + p) * this.conf.Amplitude;
                     break;
                 case "Sawtooth":
-                    outValue = waveforms.sawtooth(i / audio.samplerate * f) * this.conf.Amplitude;
+                    outValue = waveforms.sawtooth(i / audio.samplerate * f + p) * this.conf.Amplitude;
                     break;
                 case "Triangle":
-                    outValue = waveforms.triangle(i / audio.samplerate * f) * this.conf.Amplitude;
+                    outValue = waveforms.triangle(i / audio.samplerate * f + p) * this.conf.Amplitude;
                     break;
             }
             inPcm[i] += Math.pow(Math.abs(outValue), this.conf.Exponent) * Math.sign(outValue);
